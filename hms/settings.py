91df38f228d5c07d05739bb5ac0bd7e373bbcda9
eliblurr <elvissegbawu@gmail.com>
2022-04-27 17:00:05 +0000

    cors

--- hms/settings.py
+++ hms/settings.py
@@ -62,7 +62,8 @@ INSTALLED_APPS = [
     'visit',
     'patient',
     'record', 
-    'multiselectfield'
+    'multiselectfield',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -73,7 +74,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS=True
 
 ROOT_URLCONF = 'hms.urls'
 
