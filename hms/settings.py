15085993fb8379c2e90bb2f9677b902e55b78ce2
eliblurr <elvissegbawu@gmail.com>
2022-05-01 12:44:41 +0000

    debug

--- hms/settings.py
+++ hms/settings.py
@@ -117,8 +117,10 @@ WSGI_APPLICATION = 'hms.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
